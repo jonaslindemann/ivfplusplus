d8c99b7a52cb89b86a87cfbb8569d02b33c90162
jonas_lindemann <jonas_lindemann@a6de32fe-af7f-4794-a382-227ccb84ef07>
2008-04-07 21:10:31 +0000

    Small updates.

--- ivfenv.cmd
+++ ivfenv.cmd
@@ -1,5 +1,5 @@
 @echo off
-set PYTHON_ROOT=c:\python24
-set IVF_ROOT=d:\dev\ivf
-set IVF_DEPEND_ROOT=d:\dev\ivf-depend
+set PYTHON_ROOT=c:\python25
+set IVF_ROOT=c:\users\jonas\documents\development\ivf
+set IVF_DEPEND_ROOT=c:\libraries\ivf-depend
 set IVF_UTILS=%IVF_ROOT%\utils
